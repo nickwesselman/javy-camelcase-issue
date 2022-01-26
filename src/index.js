7434835fcbc1cbf145d9fb86d9896404b1c59498
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2022-01-26 10:43:20 -0500

    update repro to not require camelcase

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-import camelcase from 'camelcase';
-console.log(camelcase('hello world'));
+//import camelcase from 'camelcase';
+//console.log(camelcase('hello world'));
+
+//const UPPERCASE = /[\p{Lu}]/u;
+//const LOWERCASE = /[\p{Ll}]/u;
+//const LEADING_CAPITAL = /^[\p{Lu}](?![\p{Lu}])/gu;
+
+const IDENTIFIER = /([\p{Alpha}\p{N}_]|$)/u;
+const SEPARATORS = /[_.\- ]+/;
+const LEADING_SEPARATORS = new RegExp('^' + SEPARATORS.source);
+
+// THIS LINE APPEARS TO BE THE ISSUE
+const SEPARATORS_AND_IDENTIFIER = new RegExp(SEPARATORS.source + IDENTIFIER.source, 'gu');
+
+//const NUMBERS_AND_IDENTIFIER = new RegExp('\\d+' + IDENTIFIER.source, 'gu');
